109fadb08e0357959a2bf5e9ca3c52fefa83028c
Vitaly Lee <leevg@ukr.net>
2013-08-24 03:17:19 +0300

    t3_middleware

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,6 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from middleware_request import GetRequestsToDB
+from mock import MagicMock
+from models import RequestInfo
 
 class HttpTest(TestCase):
     def test_home(self):
@@ -10,3 +13,13 @@ class HttpTest(TestCase):
         self.assertContains(response, '42 Coffee Cups Test Assingment')
         self.assertContains(response, 'Email: admin@example.com')
         self.assertTemplateUsed(response, 'hello/home.html',)
+
+    def test_middleware(self):
+        self.gr = GetRequestsToDB()
+        self.request = MagicMock()
+        self.request.META['REQUEST_METHOD']='GET'
+        self.request.path='/'
+
+        self.assertEqual(self.gr.process_request(self.request),None)
+        req_count = RequestInfo.objects.all().count()
+        self.assertEqual(req_count,1)
