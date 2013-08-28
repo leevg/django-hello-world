7c4d24e48bca24b25af61c33c2e8e56a5be49013
Vitaly Lee <leevg@ukr.net>
2013-08-28 12:40:53 +0300

    ticket:4 fixed tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,16 +1,18 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
+from django.test.client import Client, RequestFactory
+from django.template import RequestContext
+
 from middleware_request import GetRequestsToDB
-from mock import MagicMock
 from models import RequestInfo
-from django.conf import settings
-import datetime
+
+from mock import MagicMock
+
 
 class HttpTest(TestCase):
     def test_home(self):
-        c = Client()
-        response = c.get('/')
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '42 Coffee Cups Test Assingment')
         self.assertContains(response, 'Email: admin@example.com')
@@ -20,7 +22,7 @@ class HttpTest(TestCase):
         self.gr = GetRequestsToDB()
         self.request = MagicMock()
         self.request.META['REQUEST_METHOD'] = 'GET'
-        self.request.path = '/'
+        self.request.path = reverse('home')
         self.assertEqual(self.gr.process_request(self.request), None)
         req_count = RequestInfo.objects.all().count()
         self.assertEqual(req_count, 1)
@@ -33,7 +35,6 @@ class HttpTest(TestCase):
 
 
     def test_context_processor(self):
-        response = self.client.get('/')
-        self.assertIn('settings', response.context)
-        self.assertEquals(
-            response.context['settings'].DATABASES, settings.DATABASES)
+        f = RequestFactory()
+        c = RequestContext(f.request())
+        self.assertTrue(c.get('settings') is settings)
