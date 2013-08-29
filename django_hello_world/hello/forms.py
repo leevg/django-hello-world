a8c91a1b7747a512969c97c8682341cbc6a8b7a5
Vitaly Lee <leevg@ukr.net>
2013-08-29 05:28:54 +0300

    changes

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,5 +1,17 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from models import UserInfo
+
+
+class CalendarWidget(DateInput):
+    class Media:
+        css = {
+            'all': ('css/jquery-ui-1.8.21.custom.css', )
+        }
+        js = (
+            'js/jquery.min.js',
+            'js/jquery-ui-1.8.21.custom.min.js',
+            'js/add_form.js',
+        )
 
 
 class UserInfoForm(ModelForm):
